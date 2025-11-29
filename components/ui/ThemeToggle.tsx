850bfbf6e2ebc58c9712019361df28164411c566
eunice <eunyben4@gmail.com>
2025-11-29 20:23:56 +0100

    feat(theme-toggle): persist last theme and improve UX

--- components/ui/ThemeToggle.tsx
+++ components/ui/ThemeToggle.tsx
@@ -4,6 +4,20 @@ import { useState, useEffect } from "react";
 
 const useTheme = () => {
   const [theme, setTheme] = useState<"light" | "dark">(() => {
+    if (typeof window !== 'undefined') {
+
+      const storedTheme = localStorage.getItem("theme") as "light" | "dark" | null;
+
+      const systemPrefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+      if (storedTheme) {
+        return storedTheme;
+      } else if (systemPrefersDark) {
+        return "dark";
+      } else {
+        return "light";
+      }
+    }
     return "dark";
   });
 
@@ -11,8 +25,10 @@ const useTheme = () => {
     const root = document.documentElement;
     if (theme === "dark") {
       root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
       root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     }
   }, [theme]);
 
